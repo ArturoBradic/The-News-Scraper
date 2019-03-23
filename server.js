9f47b86b510bb6ca032fd69ee116bed68d7e531a
Jorge Arturo Bradic <abradic95@email.arizona.edu>
2019-03-22 20:33:36 -0700

    / path

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ mongoose.connect("mongodb://localhost/unit18Populater", {
 });
 
 // Routes
-app.get("/", function(req, res) {
-  res.json(path.join(__dirname, "public/index.html"));
-});
+// app.get("/", function(req, res) {
+//   res.json(path.join(__dirname, "public/index.html"));
+// });
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
